e590fd835c4f6ec7b039683aacafee03665d9261
irma660 <1325455@senati.pe>
2023-08-17 16:24:30 -0500

    ingresé calculadora

--- New.java
+++ New.java
@@ -14,7 +14,26 @@ public class New {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("haihakha");
+        System.out.println("haihakha ");
+         while(true){ 
+            System.out.println("CALCULADORA");
+            System.out.println("ELIGE UNA OPCIÓN");
+            System.out.println("1.SUMA");
+            System.out.println("2.RESTA");
+            System.out.println("INGRESA OPCION - >");
+            operacion = teclado.nextInt();
+
+        }else if (operacion == 2){
+                System.out.println("Introduzca el primer número: ");
+                n1 = teclado.nextInt();
+                System.out.println("Introduzca el segundo número: ");
+                n2 = teclado.nextInt();
+
+                resta = n1 - n2;
+                System.out.println("LA RESTA ES: "+resta);
+                break;
+
+    }
         // TODO code application logic here
     }
     
